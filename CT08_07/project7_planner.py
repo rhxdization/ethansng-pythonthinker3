18efac720d17e73764237a3209b0eff9c855d173
Acu <wongethan120@gmail.com>
2026-03-19 12:48:21 +0000

    cc

--- CT08_07/project7_planner.py
+++ CT08_07/project7_planner.py
@@ -14,25 +14,51 @@ def menu():
         print("6. Exit")
         choice = input("Enter your choice: ")
         if choice == "1":
-            name = input("What would you like your task to be named? ")
-            if os.path.exists(f"{name}.txt"):
-                print(f"Task file {name}.txt already exists.")
-                confirmation = input("Overwrite? (y/n):").lower()
+            if os.path.exists(f"tasks.txt"):
+                print(f"Task file tasks.txt already exists.")
+                confirmation = input("Overwrite? (y/n): ").lower()
                 if confirmation == "y":
-                    file = open(f"{name}.txt", "w")
-                    file.close()
+                    with open("tasks.txt", "w") as file:
+                        file.write("My Task List")
+                    print("Successfully overwriten")
+                    global counter
+                    counter = 1
                 else:
-                    print("ok")
+                    print("Overwrite cancelled")
                     continue
             else:
-                file = open(f"{name}.txt", "w")
-                file.close()
+                with open("tasks.txt", "w") as file:
+                    file.write("My Task List")
+                    print("New task file written")
+                    counter = 1
         elif choice == "2":
-            print("2")
+            with open("tasks.txt", "r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    print(line.strip())
         elif choice == "3":
-            print("3")
+            task = input("Enter a new task: ")
+            with open("tasks.txt", "a") as file:
+                file.write(f"\n{counter}: {task}")
+            counter += 1
         elif choice == "4":
-            print("4")
+            with open("tasks.txt", "r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    print(line.strip())
+                delete = int(input("Enter the task number to delete: "))
+                if not isinstance(delete, int):
+                    print("type an integer")
+                else:
+                    with open("tasks.txt", "r") as file:
+                        lines = file.readlines()
+                    lines = [line for line in lines if not line.startswith(f"{str(delete)}:")]
+                    with open("tasks.txt", "w") as file:
+                        counter = 1
+                        for line in lines:
+                            file.write(f"{counter}: {line}")
+                            counter += 1
+
         elif choice == "5":
             print("5")
         elif choice == "6":
